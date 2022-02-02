915d75c125c9cb38ca281b8ca4fc59953190ab33
ZheKokha <58843273+ZheKokha@users.noreply.github.com>
2022-02-02 17:58:31 +0200

    Added utils package (#5)

--- src/test/java/com/pet/project/test/RepositoryManagementTests.java
+++ src/test/java/com/pet/project/test/RepositoryManagementTests.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import java.util.Random;
 
+import static com.pet.project.utils.StringUtil.generateRandomRepositoryNameWithPostfixLength;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -16,8 +17,7 @@ public class RepositoryManagementTests extends CommonConditions{
 
     protected static final int REPOSITORY_NAME_POSTFIX_LENGTH = 6;
     protected static final String REPOSITORY_DESCRIPTION = "auto-generated test repo";
-    private static final String ALFANUMERICAL_ALL_CAPS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static Random random = new Random();
+
 
     @Test(description = "create new repository")
     public void oneCanCreateProject() {
@@ -47,17 +47,4 @@ public class RepositoryManagementTests extends CommonConditions{
         assertTrue(isCurrentRepositoryEmpty, "newly created repository is not empty");
     }
 
-    private String getRandomString(int stringLength)
-    {
-        StringBuilder stringBuilder = new StringBuilder(stringLength);
-        for (int i = 0; i < stringLength; i++)
-        {
-            stringBuilder.append(ALFANUMERICAL_ALL_CAPS.charAt(random.nextInt(ALFANUMERICAL_ALL_CAPS.length())));
-        }
-        return stringBuilder.toString();
-    }
-
-    private String generateRandomRepositoryNameWithPostfixLength(int postfixLength){
-        return "testRepo_".concat(getRandomString(postfixLength));
-    }
 }
